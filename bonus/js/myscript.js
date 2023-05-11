19532e8c74b14a5c173a77cfd75913f68b778bb8
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-11 16:33:44 +0200

    added a second bonus with random password generation

--- bonus/js/myscript.js
+++ bonus/js/myscript.js
@@ -1,16 +1,27 @@
 
-let Name;
-let Surname;
-let FavCol;
+// ----Bonus 1----
+
+// let Name;
+// let Surname;
+// let FavCol;
+
+// function AskData(){
+//     Name = prompt("Inserisci il tuo nome:", "Es. Gabriele");
+//     Surname = prompt("Inserisci il tuo cognome:", "Es. Majocchi");
+//     FavCol = prompt("Inserisci il tuo colore preferito:", "Es. Arancione");
+//     document.getElementById("advpass").innerHTML = "La tua potentissima password è:";
+//     document.getElementById("suggpass").innerText = Name + Surname + FavCol + "23";
+//     document.getElementById("gen").innerHTML = "Genera nuovamente";
+//     document.getElementById("HiddenImg").style.display = "block";
+// }
 
 
+// ----Bonus 2----
 
 function AskData(){
-    Name = prompt("Inserisci il tuo nome:", "Es. Gabriele");
-    Surname = prompt("Inserisci il tuo cognome:", "Es. Majocchi");
-    FavCol = prompt("Inserisci il tuo colore preferito:", "Es. Arancione");
+    let result = Math.random().toString(36).substring(2,12);
     document.getElementById("advpass").innerHTML = "La tua potentissima password è:";
-    document.getElementById("suggpass").innerText = Name + Surname + FavCol + "23";
+    document.getElementById("suggpass").innerText = result;
     document.getElementById("gen").innerHTML = "Genera nuovamente";
     document.getElementById("HiddenImg").style.display = "block";
 }
