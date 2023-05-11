bb47055038a782836db5c3a43e4109aa39c1e650
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-11 17:56:20 +0200

    added third bonus with random password lenght generator

--- bonus/js/myscript.js
+++ bonus/js/myscript.js
@@ -18,8 +18,38 @@
 
 // ----Bonus 2----
 
+// function AskData(){
+//     let result = Math.random().toString(36).substring(2,12);
+//     document.getElementById("advpass").innerHTML = "La tua potentissima password è:";
+//     document.getElementById("suggpass").innerText = result;
+//     document.getElementById("gen").innerHTML = "Genera nuovamente";
+//     document.getElementById("HiddenImg").style.display = "block";
+// }
+
+// ----Bonus 3----
+
+function generateRandom(min = 7, max = 17) {
+
+    // find diff
+    let difference = max - min;
+
+    // generate random number 
+    let rand = Math.random();
+
+    // multiply with difference 
+    rand = Math.floor( rand * difference);
+
+    // add with min value 
+    rand = rand + min;
+
+    return rand;
+}
+
+let a;
+
 function AskData(){
-    let result = Math.random().toString(36).substring(2,12);
+    a = generateRandom();
+    let result = Math.random().toString(36).substring(2,a);
     document.getElementById("advpass").innerHTML = "La tua potentissima password è:";
     document.getElementById("suggpass").innerText = result;
     document.getElementById("gen").innerHTML = "Genera nuovamente";
